20b1c3dbc3a4dcecf8cbd73857d4d450e2589dcc
Jose A Dianes <jadianes@gmail.com>
2014-09-01 21:48:58 +0100

    new data

--- glm-model/train.R
+++ glm-model/train.R
@@ -2,14 +2,15 @@ library(caret)
 
 source("./io.R")
 
-
+##  Prepare train and test data
 train.ind = sample(1:dim(lime.df)[1],size=dim(lime.df)[1]/2,replace=F)
 train.lime.df = lime.df[train.ind,]
 test.lime.df = lime.df[-train.ind,]
 
+##  Fit model
 lime.model <- train(
-    Juice.Volume ~ Lime.Weight + Lime.Circumference + Lime.Weight*Lime.Circumference,
+    Juice.Volume ~ Lime.Weight + Lime.Circumference, # TODO add?  + Lime.Weight*Lime.Circumference
     method = "glm",
-    data=train.lime.df
+    data=lime.df  # TODO: eventually move to train.lime.df
 )
 
